47d38a433427e533e40d17c0e48d3f122a03a383
Thor Andreas Ellewsen <tellewsen@gmail.com>
2019-12-01 19:26:44 +0100

    Day 1 2019

---
+++ 2019/1/1.py
@@ -0,0 +1,24 @@
+import math
+
+with open('input', 'r') as f:
+    data = f.readlines()
+
+
+# Part 1
+def rocket_equation_simple(mass):
+    return math.floor(mass/3) - 2
+
+
+print("#1: ", sum(rocket_equation_simple(int(i)) for i in data))
+
+
+# Part 2
+def rocket_equation_recursive(mass):
+    if mass < 0:
+        return 0
+    else:
+        boo = rocket_equation_recursive(rocket_equation_simple(mass))
+        return rocket_equation_simple(mass) + (boo if boo > 0 else 0)
+
+
+print("#2: ", sum(rocket_equation_recursive(int(i)) for i in data))
