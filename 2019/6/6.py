7e7d08b0506bfbd96f5130996cf021e46623438c
Thor Andreas Ellewsen <tellewsen@gmail.com>
2019-12-07 16:48:22 +0100

    Day 6 done

---
+++ 2019/6/6.py
@@ -0,0 +1,17 @@
+"""
+Started this one out with a dict and recursion but hated it. Checked out reddit
+and found everyone were using this awesome package called networkx. So now this
+is obviously the way to go. Feels like cheating. I just wanted to see what my
+stupid mistake was, not get the solution handed to me with two lines of code.
+How have I never heard of this before..
+"""
+
+import networkx as nx
+
+data = [line.strip().split(")") for line in open('input', 'r').readlines()]
+g = nx.Graph(data)
+part1 = sum(nx.shortest_path_length(g, x, "COM") for x in g.nodes)
+part2 = nx.shortest_path_length(g, "YOU", "SAN") - 2
+
+print(part1)
+print(part2)
