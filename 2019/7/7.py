b7dcde023200d884ebcd8b1012f40a1b6866614b
Thor Andreas Ellewsen <tellewsen@gmail.com>
2019-12-08 14:18:51 +0100

    Clean up some day 7 stuff. Still not working of course

--- 2019/7/7.py
+++ 2019/7/7.py
@@ -41,9 +41,15 @@ class Amplifier(object):
                 index += 4
             elif str(opcode)[-1] == "3":
                 if phase_read:
-                    self.prog[self.prog[index + 1]] = self.input_value
+                    if mode[1]:
+                        self.prog[index + 1] = self.input_value
+                    else:
+                        self.prog[self.prog[index + 1]] = self.input_value
                 else:
-                    self.prog[self.prog[index + 1]] = self.phase
+                    if mode[1]:
+                        self.prog[index + 1] = self.phase
+                    else:
+                        self.prog[self.prog[index + 1]] = self.phase
                     phase_read = True
                 index += 2
             elif str(opcode)[-1] == "4":
@@ -65,52 +71,60 @@ class Amplifier(object):
                                                         self.prog[index]))
 
 
-amplifier_program = [int(i) for i in open('input').read().strip().split(',')]
+def main():
+    amplifier_program = [int(i) for i in
+                         open('input').read().strip().split(',')]
 
-phase = [0, 1, 2, 3, 4]
-phases = list(itertools.permutations(phase))
-best = [0, 0]
+    phase = [0, 1, 2, 3, 4]
+    phases = list(itertools.permutations(phase))
+    best = [0, 0]
 
-for phase in phases:
-    signal = 0
-    for val in phase:
-        input_value = signal
-        signal = Amplifier(amplifier_program.copy(), val)(input_value)
-    if signal > best[1]:
-        best = [phase, signal]
-print("Part #1:", best)
+    for phase in phases:
+        signal = 0
+        for val in phase:
+            input_value = signal
+            signal = Amplifier(amplifier_program.copy(), val)(input_value)
+        if signal > best[1]:
+            best = [phase, signal]
+    print("Part #1:", best)
 
-phase = [5, 6, 7, 8, 9]
-phases = list(itertools.permutations(phase))
+    phase = [5, 6, 7, 8, 9]
+    phases = list(itertools.permutations(phase))
 
-# test
-amplifier_program = [3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54,
-                     5, 55, 1005, 55, 26, 1001, 54,
-                     -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001,
-                     55, 1, 55, 2, 53, 55, 53, 4,
-                     53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10]
-phases = [[9, 7, 8, 5, 6]]
+    # test
+    # should have max thruster signal 139629729
+    amplifier_program = [3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007,
+                         54, 5, 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1,
+                         12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55,
+                         2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6,
+                         99, 0, 0, 0, 0, 10]
+    phases = [[9, 7, 8, 5, 6]]
 
-best = [0, 0]
-for phase in phases:
-    amplifiers = [Amplifier(amplifier_program.copy(), i) for i in phase]
-    signal = 0
-    halt = False
-    a = 0
+    best = [0, 0]
+    for phase in phases:
+        foo = amplifier_program.copy()
+        amplifiers = [Amplifier(foo, i) for i in phase]
+        signal = 0
+        halt = False
+        a = 0
 
-    while not halt:
-        # print("round:", a)
-        for amplifier in amplifiers:
-            try:
-                # print('signal_before:',signal)
-                signal = amplifier(signal)
-                print('signal_after:', signal)
-            except StopIteration:
-                halt = True
-                print("halting")
-        a += 1
-        result = [phase, signal]
-        print(result)
-        if result[1] > best[1]:
-            best = result
-print("Part #2:", best)
+        while not halt:
+            # print("round:", a)
+            for amplifier in amplifiers:
+                try:
+                    # print('signal_before:',signal)
+                    signal = amplifier(signal)
+                    print('signal_after:', signal)
+                except StopIteration:
+                    halt = True
+                    print("halting")
+            a += 1
+            result = [phase, signal]
+            print(result)
+            if result[1] > best[1]:
+                best = result
+    print("Part #2:", best)
+
+
+if __name__ == '__main__':
+    main()
