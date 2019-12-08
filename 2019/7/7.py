7f7031493f63d4c88fecf5a3c07d4c7a46bc00cc
Thor Andreas Ellewsen <tellewsen@gmail.com>
2019-12-08 18:08:34 +0100

    Day 7 done
    My mistake was to read the phase as the first input for every loop. The
    phase should ONLY be read as the first input when initializing the
    amplifiers. Every subsequent input should be the output signal from the
    previous amplifier.

--- 2019/7/7.py
+++ 2019/7/7.py
@@ -5,11 +5,11 @@ class Amplifier(object):
     def __init__(self, program, phase):
         self.prog = program
         self.phase = phase
-        self.input_values = []
+        self.index = 0
+        self.phase_read = False
 
     def __call__(self, input_value):
         self.input_value = input_value
-        phase_read = False
 
         def parse_mode(mode_op):
             mode_a = (mode_op // 100) % 10
@@ -17,58 +17,51 @@ class Amplifier(object):
             mode_c = (mode_op // 10000) % 10
             return mode_a, mode_b, mode_c
 
-        index = 0
-        while index < len(self.prog):
-            opcode = self.prog[index]
+        while self.index < len(self.prog):
+            opcode = self.prog[self.index]
             if opcode == 99:
                 raise StopIteration
 
             # Set the parameter mode
             mode = parse_mode(opcode)
 
-            a = self.prog[index + 1] if mode[0] else self.prog[
-                self.prog[index + 1]]
+            a = self.prog[self.index + 1] if mode[0] else self.prog[self.prog[self.index + 1]]
             try:
-                b = self.prog[index + 2] if mode[1] else self.prog[
-                    self.prog[index + 2]]
+                b = self.prog[self.index + 2] if mode[1] else self.prog[self.prog[self.index + 2]]
             except Exception:
                 pass
             if str(opcode)[-1] == "1":
-                self.prog[self.prog[index + 3]] = a + b
-                index += 4
+                self.prog[self.prog[self.index + 3]] = a + b
+                self.index += 4
             elif str(opcode)[-1] == "2":
-                self.prog[self.prog[index + 3]] = a * b
-                index += 4
+                self.prog[self.prog[self.index + 3]] = a * b
+                self.index += 4
             elif str(opcode)[-1] == "3":
-                if phase_read:
-                    if mode[1]:
-                        self.prog[index + 1] = self.input_value
-                    else:
-                        self.prog[self.prog[index + 1]] = self.input_value
+                if self.phase_read:
+                    self.prog[self.prog[self.index + 1]] = self.input_value
                 else:
-                    if mode[1]:
-                        self.prog[index + 1] = self.phase
-                    else:
-                        self.prog[self.prog[index + 1]] = self.phase
-                    phase_read = True
-                index += 2
+                    self.prog[self.prog[self.index + 1]] = self.phase
+                    self.phase_read = True
+                self.index += 2
             elif str(opcode)[-1] == "4":
-                # print('returning', a)
+                self.index += 2
                 return a
             elif str(opcode)[-1] == "5":
-                index = b if a != 0 else index + 3
+                self.index = b if a != 0 else self.index + 3
             elif str(opcode)[-1] == "6":
-                index = b if a == 0 else index + 3
+                self.index = b if a == 0 else self.index + 3
             elif str(opcode)[-1] == "7":
-                self.prog[self.prog[index + 3]] = 1 if a < b else 0
-                index += 4
+                self.prog[self.prog[self.index + 3]] = 1 if a < b else 0
+                self.index += 4
             elif str(opcode)[-1] == "8":
-                self.prog[self.prog[index + 3]] = 1 if a == b else 0
-                index += 4
+                self.prog[self.prog[self.index + 3]] = 1 if a == b else 0
+                self.index += 4
             else:
                 raise Exception(
-                    "Weird value at pos {}: {} ".format(index,
-                                                        self.prog[index]))
+                    "Weird value at pos {}: {} ".format(self.index, self.prog[self.index]))
+
+            # if self.index > len(self.prog):
+            #     self.index -= len(self.prog)
 
 
 def main():
@@ -88,41 +81,23 @@ def main():
             best = [phase, signal]
     print("Part #1:", best)
 
-    phase = [5, 6, 7, 8, 9]
-    phases = list(itertools.permutations(phase))
-
-    # test
-    # should have max thruster signal 139629729
-    amplifier_program = [3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007,
-                         54, 5, 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1,
-                         12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55,
-                         2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6,
-                         99, 0, 0, 0, 0, 10]
-    phases = [[9, 7, 8, 5, 6]]
-
+    phases = list(itertools.permutations([5, 6, 7, 8, 9]))
     best = [0, 0]
     for phase in phases:
-        foo = amplifier_program.copy()
-        amplifiers = [Amplifier(foo, i) for i in phase]
+        amplifiers = []
+        for i in range(len(phase)):
+            amplifiers.append(Amplifier(amplifier_program.copy(), phase[i]))
         signal = 0
         halt = False
-        a = 0
-
         while not halt:
-            # print("round:", a)
-            for amplifier in amplifiers:
-                try:
-                    # print('signal_before:',signal)
+            try:
+                for j, amplifier in enumerate(amplifiers):
                     signal = amplifier(signal)
-                    print('signal_after:', signal)
-                except StopIteration:
-                    halt = True
-                    print("halting")
-            a += 1
-            result = [phase, signal]
-            print(result)
-            if result[1] > best[1]:
-                best = result
+            except StopIteration:
+                halt = True
+        result = [phase, signal]
+        if result[1] > best[1]:
+            best = result
     print("Part #2:", best)
 
 
