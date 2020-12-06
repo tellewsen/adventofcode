b85b307e0f5ea3b7da77d71ede5a5666c76cdd19
Thor Andreas Ellewsen <tellewsen@gmail.com>
2020-12-06 09:35:02 +0100

    Simplify day6 solution

--- 2020/6/6.py
+++ 2020/6/6.py
@@ -1,29 +1,21 @@
 with open('input') as f:
     file = f.read().rstrip().split('\n\n')
 
+groups = [[set(b) for b in a.split('\n')] for a in file]
+
+
+def counting_func(groups, set_op):
+    sub_counts = []
+    for group in groups:
+        start = group[0]
+        for sub_group in group[1:]:
+            start = set_op(start, sub_group)
+        sub_counts.append(len(start))
+    return sum(sub_counts)
+
+
 # Part 1
-groups = [a.replace('\n', '') for a in file]
-b = [set() for _ in groups]
-for i in range(len(groups)):
-    for j in groups[i]:
-        b[i].add(j)
-print(sum([len(i) for i in b]))
+print(counting_func(groups, lambda x, y: x | y))
 
 # Part 2
-part2 = [a.split('\n') for a in file]
-
-
-def char_in_rest(char, rest):
-    in_all = True
-    for entry in rest:
-        if char not in entry:
-            in_all = False
-            break
-    return in_all
-
-
-in_rest = []
-for group in part2:
-    for character in group[0]:
-        in_rest.append(char_in_rest(character, group[0:]))
-print(sum(in_rest))
+print(counting_func(groups, lambda x, y: x & y))
