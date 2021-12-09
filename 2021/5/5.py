9f3cc6e9cf31b0f1d5fe6c744f6ab3a80e5abf60
Andreas Ellewsen <andreas@ellewsen.no>
2021-12-09 21:23:52 +0100

    Complete day 5 2021

--- 2021/5/5.py
+++ 2021/5/5.py
@@ -1,24 +1,54 @@
-def p1(a):
-    print(a)
-    return
+import numpy as np
 
 
-def p2():
-    return
+def read_file(filename):
+    with open(filename, "r") as f:
+        data = [i.rstrip() for i in f.readlines()]
+    return data
 
 
-with open("input.txt", "r") as f:
-    data = [i.rstrip() for i in f.readlines()]
+def mark_points(grid, start, end, skip_diagonal):
+    x, y = start
+    x_e, y_e = end
+    dx = -1 if x > x_e else 1 if x < x_e else 0
+    dy = -1 if y > y_e else 1 if y < y_e else 0
+    if skip_diagonal:
+        if dx != 0 and dy != 0:
+            return
+    grid[x, y] += 1
+    while (x, y) != end:
+        x += dx
+        y += dy
+        grid[x, y] += 1
 
-for line in data:
-    start, stop = line.split(" -> ")
-    x1, y1 = start.split(",")
-    x2, y2 = stop.split(",")
-    x1 = int(x1)
-    x2 = int(x2)
-    y1 = int(y1)
-    y2 = int(y2)
-    gradient = (y2 - y1) / (x2 - x1)
 
-print(1, p1(data))
-print(2, p2())
+def parse_content(content, skip_diagonal):
+    grid = np.zeros((1000, 1000), dtype=int)
+    for line in content:
+        start, stop = line.split(" -> ")
+        x1, y1 = start.split(",")
+        x2, y2 = stop.split(",")
+        start = int(x1), int(y1)
+        end = int(x2), int(y2)
+        mark_points(grid, start, end, skip_diagonal)
+    return grid
+
+
+def p1(content):
+    parsed = parse_content(content, skip_diagonal=True)
+    return (parsed > 1).sum()
+
+
+def p2(content):
+    parsed = parse_content(content, skip_diagonal=False)
+    return (parsed > 1).sum()
+
+
+def main():
+    content = read_file("input.txt")
+    print(1, p1(content))
+    print(2, p2(content))
+
+
+if __name__ == '__main__':
+    main()
