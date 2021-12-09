53c94616793e995e111b3ca881fe1a883bd62920
Andreas Ellewsen <andreas@ellewsen.no>
2021-12-09 23:02:18 +0100

    Add day 7 2021

---
+++ 2021/7/7.py
@@ -0,0 +1,33 @@
+def read_file(filename):
+    with open(filename, "r") as f:
+        data = f.readlines()[0].rstrip().split(',')
+    return [int(i) for i in data]
+
+
+def get_ans(content, cost):
+    minimum = min(content)
+    maximum = max(content)
+    answer = None
+    for i in range(minimum, maximum+1):
+        ans = sum(cost(abs(j - i)) for j in content)
+        if answer is None or ans < answer:
+            answer = ans
+    return answer
+
+
+def p1(content):
+    return get_ans(content, lambda x: x)
+
+
+def p2(content):
+    return get_ans(content, lambda x: x*(x+1)/2)
+
+
+def main():
+    content = read_file('input.txt')
+    print(1, p1(content))
+    print(2, p2(content))
+
+
+if __name__ == '__main__':
+    main()
