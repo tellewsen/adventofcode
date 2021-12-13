f6afafcf20dabaf1c542e99b03c9183c036a6aaf
Andreas Ellewsen <andreas@ellewsen.no>
2021-12-13 23:08:40 +0100

    Add rest of day 10 2021
    Mid points are stupid

--- 2021/10/10.py
+++ 2021/10/10.py
@@ -1,5 +1,4 @@
-import numpy as np
-import functools
+import math
 
 
 def read_file(filename):
@@ -8,48 +7,47 @@ def read_file(filename):
     return data
 
 
-POINT_MAP = {")": 3, "]": 57, "}": 1197, ">": 25137}
-open_char = {"(": ")", "[": "]", "{": "}", "<": ">"}
-close_char = {v: k for k, v in open_char.items()}
-POINT_MAP2 = {")": 1, "]": 2, "}": 3, ">": 4}
+OPEN2CLOSE = {"(": ")", "[": "]", "{": "}", "<": ">"}
 
 
 def get_first_illegal_char(line):
     waiting_for = []
     for char in line:
-        if char in open_char.keys():  # opening char
+        if char in OPEN2CLOSE:  # opening char
             waiting_for.append(char)
         else:  # closing_char
-            if waiting_for[-1] == close_char[char]:
+            if char == OPEN2CLOSE[waiting_for[-1]]:
                 waiting_for.pop()
             else:
                 return char
 
 
-def p1(hm):
+def p1(lines):
+    point_map = {")": 3, "]": 57, "}": 1197, ">": 25137}
     illegal_chars = []
-    for line in hm:
+    for line in lines:
         illegal_char = get_first_illegal_char(line)
         if illegal_char:
             illegal_chars.append(illegal_char)
-    return sum([POINT_MAP[i] for i in illegal_chars])
+    return sum([point_map[i] for i in illegal_chars])
 
 
 def get_missing_chars(line):
     waiting_for = []
     for char in line:
-        if char in open_char.keys():  # opening char
+        if char in OPEN2CLOSE.keys():  # opening char
             waiting_for.append(char)
         else:  # closing_char
             waiting_for.pop()
-    return ''.join([open_char[i] for i in reversed(waiting_for)])
+    return ''.join([OPEN2CLOSE[i] for i in reversed(waiting_for)])
 
 
 def calculate_score(chars):
+    point_map = {")": 1, "]": 2, "}": 3, ">": 4}
     score = 0
     for char in chars:
         score *= 5
-        score += POINT_MAP2[char]
+        score += point_map[char]
     return score
 
 
@@ -57,17 +55,14 @@ def p2(content):
     incomplete = [i for i in content if not get_first_illegal_char(i)]
     missing_chars = [get_missing_chars(i) for i in incomplete]
     scores = [calculate_score(i) for i in missing_chars]
-    mid = round(len(scores)/2)
-    print(scores)
+    mid = math.floor(len(scores)/2)
     return sorted(scores)[mid]
 
 
 def main():
-    filename = "input.txt"
-    #filename = "example_input.txt"
-    hm = read_file(filename)
-    print(1, p1(hm))
-    print(2, p2(hm))
+    lines = read_file("input.txt")
+    print(1, p1(lines))
+    print(2, p2(lines))
 
 
 if __name__ == "__main__":
