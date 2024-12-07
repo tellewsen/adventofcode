de7531d7a853be4294a4ed675fd46bf5b7e17472
Andreas Ellewsen <andreas@ellewsen.no>
2024-12-07 18:45:47 +0100

    Add day 6 part 1

--- python/2024/6.py
+++ python/2024/6.py
@@ -1,30 +1,22 @@
 def solver(text):
-    visitmark = "*"
     grid = text.splitlines()
-    dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]  # up, right, down, left
+    dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # up, right, down, left
     dir_idx = 0
     for i in range(len(grid)):
         if "^" in grid[i]:
-            pos = [i, grid[i].index("^")]
+            pos = (i, grid[i].index("^"))
             break
-
-    grid[pos[0]] = grid[pos[0]][: pos[1]] + visitmark + grid[pos[0]][pos[1] + 1 :]
+    rows = len(grid)
+    cols = len(grid[0])
+    visited = set()
+    visited.add(pos)
     while True:
-        print(pos)
-        try:
-            if grid[pos[0]][pos[1]] == "#":
-                dir_idx += 1
-                dir_idx = dir_idx % 3
-                print(dir_idx)
-                continue
-        except IndexError:
+        next_pos = (pos[0] + dirs[dir_idx][0], pos[1] + dirs[dir_idx][1])
+        if not (0 <= next_pos[0] < rows) or not (0 <= next_pos[1] < cols):
             break
-        pos[0] += dirs[dir_idx][0]
-        pos[1] += dirs[dir_idx][1]
-        try:
-            grid[pos[0]] = (
-                grid[pos[0]][: pos[1]] + visitmark + grid[pos[0]][pos[1] + 1 :]
-            )
-        except IndexError:
-            break
-    return sum(i.count(visitmark) for i in grid), 1
+        if grid[next_pos[0]][next_pos[1]] == "#":
+            dir_idx = (dir_idx + 1) % 4
+        else:
+            pos = next_pos
+            visited.add(pos)
+    return len(visited), 1
