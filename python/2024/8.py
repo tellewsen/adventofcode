9fde1dd1e4e3cd94903d92072d3f3fca1b96964b
Andreas Ellewsen <andreas.ellewsen@itera.com>
2024-12-24 14:26:34 +0100

    Add part 2 of day 8 2024

--- python/2024/8.py
+++ python/2024/8.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
-from itertools import combinations
+from itertools import combinations, product
+import math
 
 
 def solver(text):
@@ -12,17 +13,36 @@ def solver(text):
             if data[r][c] != ".":
                 antennas[data[r][c]].append((r, c))
     antinodes = set()
+    extras = set()
     for k, vs in antennas.items():
         combos = combinations(vs, 2)
-        for s in combos:
-            a, b = s
-            xdiff = b[0] - a[0]
-            ydiff = b[1] - a[1]
-            c1 = (a[0] - xdiff, a[1] - ydiff)
-            c2 = (b[0] + xdiff, b[1] + ydiff)
-            print(vs, s, xdiff, ydiff, c1, c2)
-            if 0 <= c1[0] < rows and 0 <= c1[1] < cols:
+        for a, b in combos:
+            dx = b[0] - a[0]
+            dy = b[1] - a[1]
+            # part 1
+            c1 = (a[0] - dx, a[1] - dy)
+            if 0 <= c1[0] < cols and 0 <= c1[1] < rows:
                 antinodes.add(c1)
-            if 0 <= c2[0] < rows and 0 <= c2[1] < cols:
+            c2 = (b[0] + dx, b[1] + dy)
+            if 0 <= c2[0] < cols and 0 <= c2[1] < rows:
                 antinodes.add(c2)
-    return len(antinodes), 1
+            # Part 2
+            gcd = math.gcd(dx, dy)
+            dx = dx / gcd
+            dy = dy / gcd
+            c1 = (a[0] - dx, a[1] - dy)
+            c2 = (a[0] + dx, a[1] + dy)
+            while 0 <= c1[0] < cols and 0 <= c1[1] < rows:
+                extras.add(c1)
+                c1 = (c1[0] - dx, c1[1] - dy)
+            while 0 <= c2[0] < cols and 0 <= c2[1] < rows:
+                extras.add(c2)
+                c2 = (c2[0] + dx, c2[1] + dy)
+            extras.add(a)
+            extras.add(b)
+    return len(antinodes), len(extras)
+
+
+# 931 low
+# 951 low
+# 1009 low
